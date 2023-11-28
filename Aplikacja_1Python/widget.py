0b93b2e333b3a7b981ae244ce952ccfae4d04c77
Jakub Bugała <275637@student.pwr.edu.pl>
2023-11-28 16:38:57 +0100

    Update widget.py

--- Aplikacja_1Python/widget.py
+++ Aplikacja_1Python/widget.py
@@ -1,12 +1,8 @@
-# This Python file uses the following encoding: utf-8
+
 import sys
 
 from PySide6.QtWidgets import QApplication, QWidget
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_Widget
 
 class Widget(QWidget):
